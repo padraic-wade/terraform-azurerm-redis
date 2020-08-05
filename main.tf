5dbb9be32a31d29f1b5e91726d13c0eaec1bd0b2
Emery <EmerWe01@risk.regn.net>
2020-08-05 07:28:30 -0500

    added new local variable

--- main.tf
+++ main.tf
@@ -5,20 +5,21 @@ locals {
     Standard = "C",
     Premium  = "P"
   }
+  name = "${data.azurerm_subscription.subscription.display_name}-${var.location}-${var.app_name}"
 }
 data "azurerm_subscription" "subscription" {
   }
 
 resource "azurerm_resource_group" "redis_group" {
   location = var.location
-  name = "rg-${data.azurerm_subscription.subscription.display_name}-${var.location}-${var.app_name}"
+  name = "rg-${local.name}"
 }
 
 resource "azurerm_redis_cache" "redis_cache" {
   capacity = 0
   family = lookup(local.redis_family_map, var.sku_name)
   location = var.location
-  name = "redis-${data.azurerm_subscription.subscription.display_name}-${var.location}-${var.redis_name}"
+  name = "redis-${local.name}-${var.redis_name}"
   resource_group_name = azurerm_resource_group.redis_group.name
   sku_name = var.sku_name
   minimum_tls_version = var.tls_version
